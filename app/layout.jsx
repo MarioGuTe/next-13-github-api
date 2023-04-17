dccbe01ae9ad01564ee40a6f818e6a0a433e87fa
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-16 22:12:51 -0400

    add Poppins font import

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,10 @@
+import { Poppins } from "next/font/google";
 import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Next13 Github Api Project",
@@ -9,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
