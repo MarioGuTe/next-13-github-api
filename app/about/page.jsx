7f9cde82ef9bdf439c150b8bd210cf42426bcd86
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-16 22:04:33 -0400

    first commit

---
+++ app/about/page.jsx
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: "About Page",
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About me</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores rem,
+        eveniet recusandae fugit ipsum neque, inventore laboriosam quaerat
+        soluta magnam culpa sequi cupiditate fugiat error cum, necessitatibus
+        placeat ratione tenetur.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,23 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius odio
+        reprehenderit incidunt, harum, recusandae atque quibusdam architecto
+        repudiandae amet rerum soluta, delectus corrupti. Eos id commodi quasi
+        praesentium molestiae cupiditate. Quod qui omnis sed a nesciunt, autem
+        temporibus eaque blanditiis minima doloremque ad sit. Sed, animi alias
+        ut modi in nostrum quis numquam maxime! Dolor provident repellendus
+        autem expedita animi! Cum nihil vero hic quidem a voluptates debitis
+        officia quisquam quis deleniti iusto delectus necessitatibus explicabo
+        suscipit in deserunt, quia exercitationem. Facilis laboriosam ad
+        quisquam dolor nam in. Sapiente, iure. Autem sapiente dolorum quisquam
+        aspernatur explicabo excepturi adipisci libero similique iste velit
+        nostrum sit eaque magnam nesciunt provident omnis ducimus, accusamus
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
